79f649358747b831f46052f2ae98039862dde9bf
Bo Carey <bo.carey@outlook.com>
2020-04-18 21:58:12 -0400

    basic query testing

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var jsdom = requre('jsdom');
+var jsdom = require('jsdom');
 const { JSDOM } = jsdom;
+const { window } = new JSDOM();
 const { document } = (new JSDOM('')).window;
 global.document = document;
 var $ = jQuery = require('jquery')(window);
 
-var discordKey = '';
 var earningsKey = '';
 
-
+let getInfo = (symbol) => {
+    $.get(`https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=margin&token=${earningsKey}`, data => {
+        console.log('data :', JSON.parse(data));
+    });
+}
+getInfo('AAPL');
