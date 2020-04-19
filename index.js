0e76169d9e71a3cbbe6a5b7bbeec60ad3bdd4dc7
Bo Carey <bo.carey@outlook.com>
2020-04-19 00:49:29 -0400

    added a few functions for the discord client
    interactions are looking good, now it is time to start getting data and developing some actual use cases before it gets too far into the weeds.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const auth = require('./auth.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var jsdom = require('jsdom');
@@ -7,11 +8,38 @@ const { document } = (new JSDOM('')).window;
 global.document = document;
 var $ = jQuery = require('jquery')(window);
 
-var earningsKey = '';
-
-let getInfo = (symbol) => {
-    $.get(`https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=margin&token=${earningsKey}`, data => {
-        console.log('data :', JSON.parse(data));
+let getInfo = (metric, symbol, msg) => {
+    if (!['price', 'valuation', 'growth', 'margin', 'management', 'financialStrength', 'perShare'].includes(metric)) 
+        return msg.reply(`That didn't work. Try 'price', 'valuation', 'growth', 'margin', 'management', 'financialStrength' or 'perShare'`);
+    $.get(`https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=${metric}&token=${auth.earningsKey}`, data => {
+        msg.reply('i have data about ' + symbol);
     });
 }
-getInfo('AAPL');
+
+let getFinancials = (metric, symbol, msg) => {
+    msg.reply('jk');
+}
+
+client.on('ready', () => {
+    console.log('all logged in!!!!');
+});
+
+client.on('message', msg => { 
+    if(msg.author.bot) return;
+    if (msg.content.substring(0, 1) !== '!') return;
+    let phrases = msg.content.substring(1).split(' ');
+    console.log('phrases :', phrases);
+    let command = phrases[0];
+    switch(command) {
+        case 'info':
+            getInfo(phrases[1], phrases[2], msg);
+            break;
+        case 'fin':
+            getFinancials(phrases[1], phrases[2], msg);
+            break;
+        default:
+            msg.reply(`I currently support 'fin' for financials, 'info' for metrics. Try '!info growth AAPL'`);
+    }
+});
+
+client.login(auth.token);
