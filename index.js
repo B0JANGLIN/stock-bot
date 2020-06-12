0b22cc6db1aec4a86d3156f50544366425bfec77
Bo Carey <bo.carey@outlook.com>
2020-06-11 23:45:03 -0400

    Initial commit with graphql working

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ const auth = require('./auth.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const MessageRouter = require('./message-router.js');
-var fs = require('fs');
+
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+var root = {
+    hello: () => {
+        return 'Hello World!';
+    },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 
 client.on('ready', () => {
     console.log('client::ready');
